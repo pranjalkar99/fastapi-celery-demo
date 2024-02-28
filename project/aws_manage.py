bf2615f9fc1095b1395ba1cf2ec1f149d3915caf
pranjalkar99 <pranjalkar99@gmail.com>
2024-02-29 04:37:10 +0900

    trying to debug aws upload

--- project/aws_manage.py
+++ project/aws_manage.py
@@ -5,9 +5,7 @@ import boto3
 from botocore.exceptions import NoCredentialsError
 load_dotenv()
 import logging
-log_file_path = 'celery_logs_aws.log'
-logging.basicConfig(filename=log_file_path,level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
+logger = logging.getLogger(__name__)
 
 
 def upload_to_s3(local_filename, s3_bucket, s3_folder):
@@ -19,11 +17,11 @@ def upload_to_s3(local_filename, s3_bucket, s3_folder):
     try:
         s3.upload_file(local_filename, s3_bucket, f"{s3_folder}/{os.path.basename(local_filename)}")
         s3_url = f"https://{s3_bucket}.s3.amazonaws.com/{s3_folder}/{os.path.basename(local_filename)}"
-        logging.info(f"Uploaded {local_filename} to {s3_url}")
+        logger.info(f"Uploaded {local_filename} to {s3_url}")
         return s3_url
     except NoCredentialsError as e:
         error_message = f"Credentials not available: {e}"
-        logging.error(error_message)
+        logger.error(error_message)
         raise Exception(error_message)
 
 def upload_images_to_s3(input_folder, output_folder, s3_bucket):
@@ -37,11 +35,14 @@ def upload_images_to_s3(input_folder, output_folder, s3_bucket):
                 s3_urls.append(s3_url)
             else:
                 error_message = f"Failed to upload {local_filepath} to S3"
-                logging.error(error_message)
+                logger.error(error_message)
                 raise Exception(error_message)
         except Exception as e:
-            logging.error(f"Error during S3 upload for {local_filepath}: {e}")
+            logger.error(f"Error during S3 upload for {local_filepath}: {e}")
             raise e
 
-    logging.info(f"All files in {input_folder} successfully uploaded to S3. URLs: {s3_urls}")
+    logger.info(f"All files in {input_folder} successfully uploaded to S3. URLs: {s3_urls}")
     return s3_urls
+
+if __name__ =="__main__":
+    upload_images_to_s3('pk_out_debug','pk_out_debug_213jxj','ovvy-ml-output')
